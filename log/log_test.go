caf44b9cbe87a94681526b86508fc047c19f5baf
palanceli <24962764@qq.com>
2019-02-24 21:40:48 +0800

    commit changes

---
+++ log/log_test.go
@@ -0,0 +1,21 @@
+package gollog
+
+import (
+	"flag"
+	"testing"
+
+	"github.com/golang/glog"
+)
+
+func TestBasicUsage(t *testing.T) {
+	defer glog.Flush() // 退出之前Flush残余log数据
+
+	flag.Set("logtostderr", "true") // 打印到stderr
+	flag.Parse()
+	glog.Info("testing info")
+
+	// 若使用go test -run="BasicUsage"  -v=2会报错：
+	// go test: illegal bool flag value 2
+	glog.V(2).Infoln("testing V2 info")
+	// 如果是普通的应用不存在此问题
+}
--- main.go
+++ main.go
@@ -4,8 +4,48 @@ import (
 	"github.com/golang/glog"
 )
 
+type Server struct {
+	x, y int
+}
+
+func (s *Server) Start() {
+	glog.Info("start server...")
+}
+
+type Proc struct {
+	z int
+}
+
+func (p *Proc) Start() {
+	glog.Info("start proc...")
+}
+
+type Instance struct {
+	x, y   int
+	server *Server
+	proc   *Proc
+}
+
+type Service interface {
+	Start()
+}
+
+func StartService(s Service) {
+	s.Start()
+}
+
 func main() {
 	defer glog.Flush()
-
-	glog.V(8).Infof("test glog.V")
+	obj := &Instance{
+		x: 0,
+		y: 1,
+		server: &Server{
+			x: 2,
+			y: 3,
+		},
+		proc: &Proc{
+			z: 4,
+		},
+	}
+	StartService(obj.server)
 }
