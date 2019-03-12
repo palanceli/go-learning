b6a05fa1d0a72b8596daf302154d6828e013c193
palance <palanceli@163.com>
2019-03-12 23:00:37 +0800

    commit changes

--- yaml/yaml_test.go
+++ yaml/yaml_test.go
@@ -1,11 +1,15 @@
 package golyaml
 
+// 参考资料可参见https://godoc.org/gopkg.in/yaml.v2
+
 import (
 	"flag"
+	"io/ioutil"
 	"os"
 	"testing"
 
 	"github.com/golang/glog"
+	"gopkg.in/yaml.v2"
 )
 
 func TestMain(m *testing.M) {
@@ -16,6 +20,33 @@ func TestMain(m *testing.M) {
 	os.Exit(ret)
 }
 
-func TestMarshal(t *testing.T) {
+type Score struct {
+	ClassName  string `yaml:"class_name"`
+	ClassScore uint16 `yaml:"class_score"`
+}
 
+type Student struct {
+	Name  string  `yaml:"name"`
+	Age   uint32  `yaml:"age"`
+	Sex   string  `yaml:"sex"`
+	Score []Score `yaml:"score"`
+}
+
+// 从yml中读入结构体Student
+func TestMarshal(t *testing.T) {
+	ymlPath := "./test.yml"
+	glog.Infof("Reading config file %s ...", ymlPath)
+	data, err := ioutil.ReadFile(ymlPath)
+	if err != nil {
+		glog.Fatalf("FAILED to read config file %s. err = %v", ymlPath, err)
+	}
+
+	glog.Infof("Parsing config file %s ...", ymlPath)
+	student := Student{}
+	err = yaml.Unmarshal(data, &student)
+	if err != nil {
+		glog.Fatalf("FAILED parse config file %s. err = %v", ymlPath, err)
+	}
+
+	glog.Infof("config = %v ", student)
 }
