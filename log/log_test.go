1a733ad0b6312925deb3508588f008d1b030e1cd
libei <libei@windimg.com>
2019-09-26 11:28:22 +0800

    add changes

--- log/log_test.go
+++ log/log_test.go
@@ -1,10 +1,12 @@
 package gollog
 
 import (
+	// "errors"
 	"flag"
 	"testing"
 
 	"github.com/golang/glog"
+	"github.com/pkg/errors"
 )
 
 /*
@@ -23,4 +25,48 @@ func TestBasicUsage(t *testing.T) {
 	// go test: illegal bool flag value 2
 	glog.V(2).Infoln("testing V2 info")
 	// 如果是普通的应用不存在此问题
+}
+
+func errorWrapA() (err error) {
+	err = errors.New("create error for some reason")
+	return
+}
+
+func errorWrapB() (err error) {
+	err = errorWrapA()
+	if err != nil {
+		// err = errors.Wrap(err, "FAILED to errorWrapA")
+		return
+	}
+	return
+}
+
+func errorWrapC() (err error) {
+	err = errorWrapB()
+	if err != nil {
+		// err = errors.Wrap(err, "FAILED to errorWrapB")
+		return
+	}
+	return
+}
+
+type stackTracer interface {
+	StackTrace() errors.StackTrace
+}
+
+func TestErrorWrap(t *testing.T) {
+	err := errorWrapC()
+	glog.Errorf("%+v", err)
+	// err = errors.Cause(err)
+	// glog.Errorf("%v", err)
+	// if err != nil {
+	// 	if err, ok := err.(stackTracer); ok {
+	// 		glog.Errorf("err=%v", err)
+	// 		for _, f := range err.StackTrace() {
+	// 			glog.Errorf("%+v\n", f)
+	// 		}
+	// 	} else {
+	// 		glog.Errorf("err=%v", err)
+	// 	}
+	// }
 }
